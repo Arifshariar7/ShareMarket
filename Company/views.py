c822e93a3d1cb7a1a2d84d5ae309722103c34398
Arifshariar7 <arifshariar7@gmail.com>
2023-04-02 03:28:51 +0600

    [ADD] score1

--- Company/views.py
+++ Company/views.py
@@ -5,6 +5,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
+
+from .models import Company,Yearly
+from django.db.models import F,Subquery, OuterRef
+from django.db.models.functions import Round,RowNumber
+
+
+
 # from .models import Theme
 
 def home(request):
@@ -15,4 +22,19 @@ def home(request):
 
 def magic_formula(request):
     # stocks = Stock.objects.order_by('-mf_rank')
-    return render(request, 'company/magic_formula.html', {})
+    subquery = Yearly.objects.filter(company_id=OuterRef('pk')).order_by('id').values('npat')[:1]
+
+    companies = Company.objects.all().annotate(
+        ROE=Round(
+            (Subquery(
+                subquery
+            ) / F('shareholders_equity')) * 100, 
+            3
+        )
+    ).order_by('-ROE')
+
+    score = 1
+    for company in companies:
+        company.score1 = score
+        score += 1    
+    return render(request, 'company/magic_formula.html', {'companies':companies})
